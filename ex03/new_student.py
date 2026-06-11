f0cf560ac8cbbc2090fe0af7e40f3d5c377c3121
lmatkows <lmatkows@student.42perpignan.fr>
2026-06-11 10:27:43 +0200

    terminado

---
+++ ex03/new_student.py
@@ -0,0 +1,27 @@
+import random
+import string
+from dataclasses import dataclass, field
+
+
+def generate_id() -> str:
+    """Generate a random ID
+    """
+    return "".join(random.choices(string.ascii_lowercase, k=15))
+
+
+@dataclass
+class Student:
+    """A class representing a student
+    """
+    name: str
+    surname: str
+    active: bool = field(default=True)
+    login: str = field(init=False)
+    id: str = field(init=False)
+
+    def __post_init__(self):
+        """Generate the attributes that use functions
+        In this case id and login
+        """
+        self.id = generate_id()
+        self.login = self.name[0].capitalize() + self.surname.lower()
